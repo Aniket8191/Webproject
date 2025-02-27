1e4e5a59be6cf8346164a55e03c25b1554d3a952
Aniket8191 <162547486+Aniket8191@users.noreply.github.com>
2025-02-27 18:42:37 +0530

    Initial commit - React App

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+// src/components/Product.js
+function Product() {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Explore our latest sneakers collection!</p>
+    </div>
+  );
+}
+
+export default Product;
